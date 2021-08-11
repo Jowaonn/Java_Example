ddb6f93c95c18aab17c968f6729dea6c1d7f7f12
Jowaonn <624230041@webmail.npru.ac.th>
2021-08-11 10:40:10 +0700

    Update data 1

--- MyFirstJava.java
+++ MyFirstJava.java
@@ -1,5 +1,6 @@
 public class MyFirstJava{
     public static void main(String[] args) {
-        System.out.println("My first Java Program");
+        System.out.println("update data");
+        System.out.println("Gti command");
     }
 }
