5e1fb6bd375e927983fda06d9d96b12e5ff06ea9
Jowaonn <624230041@webmail.npru.ac.th>
2021-08-11 11:24:01 +0700

    Update Data 2

--- MyFirstJava.java
+++ MyFirstJava.java
@@ -1,6 +1,7 @@
 public class MyFirstJava{
     public static void main(String[] args) {
-        System.out.println("update data");
-        System.out.println("Gti command");
+        System.out.println("My name is : Amonthep Suwannawek");
+        System.out.println("ID : 624230041");
+        System.out.println("My :class 62/38");
     }
 }
